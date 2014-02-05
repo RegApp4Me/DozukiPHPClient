2b75e705f1940f40f58c1218e2ec4e6d7c19d3fc
Daniel Lawson <corexian@gmail.com>
2014-02-04 23:22:12 -0600

    Add requirements, documentation, license, and misc

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ## Description ##
 This is a PHP client library for accessing the Dozuki V2.0 API (https://www.dozuki.com/api/2.0/doc/)
+
+## Requirements ##
+PHP 5.3 or higher
+Tests depend on PHPUnit (http://www.phpunit.de/manual/current/en/installation.html)
---
+++ tests/DozukiTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Dozuki PHP Client
+ *
+ * Copyright (c) 2014 WhyteSpyder Inc.
+ *
+ * @package DozukiPHPClient
+ * @author  Daniel Lawson <corexian@gmail.com>
+ * @license MIT
+ * @link https://github.com/WhyteSpyder/DozukiPHPClient/
+ **/
+namespace WhyteSpyder\DozukiPHPClient;
+
+class DozukiTest extends \PHPUnit_Framework_TestCase {
+}
